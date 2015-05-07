36ab7164980f59cb982b1970203c326fed2d6a13
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-05-07 14:49:39 -0400

    generate Contributor and Postal Code samples for review

--- linking_accuracy/generate_samples_for_review.R
+++ linking_accuracy/generate_samples_for_review.R
@@ -1,7 +1,7 @@
-source("../lib/constants.R")
 source("../lib/record_linking/link_accuracy.R")
+source("../lib/constants.R")
 
-library(plyr); library(dplyr)
+library(dplyr)
 library(GetoptLong)
 
 config <- within(list(), {
@@ -11,7 +11,7 @@ config <- within(list(), {
 
   sourceDir <- paste("../linked_data/", dataStoreDir, sep="")
   print(paste("The source directory is", sourceDir))
-  targetDir <- dataStoreDir
+  targetDir <- paste("samples/", dataStoreDir, sep="")
   print(paste("The target directory is", targetDir))
 })
 
@@ -23,13 +23,73 @@ data <- ContributionsDataWrapper(
   )
 )
 
-# maxmum probability that a postal code has distinct contributor_ids for
-# the same contributor (i.e. the records were not linked successfully)
-maxProbPcodeHasMissingLinks <-
-  data$PostalCodesWithMultipleContributorIdsCount() /
-    length(levels(data$set$postal_code))
+data$pcodes <- levels(data$set$postal_code)
+data$maxContributorId <- max(data$set$contributor_id)
 
-# maxmum probability that a contributor will have misassigned records
-# (i.e records for two different contributors were mistakenly linked)
-maxProbContributorHasMisassignedRecord <-
-  data$ContribIdsWithMultipleNamesCount() / max(data$set$contributor_id)
+sP <- list(kConfidence = 0.99, kInterval = 0.05)
+
+print("Generating postal_code sample parameters...")
+# maxmum probability that a postal code has 'missing links' (i.e. distinct
+# contributor_ids for the same contributor)
+sP$maxProbPcodeHasMissingLinks <-
+  data$PostalCodesWithMultipleContributorIdsCount() / length(data$pcodes)
+
+# minimum number of postal_codes to sample to be able to infer the proportion
+# that have 'missing links'.
+sP$missingLinksSampleSize <- MinSampleSizeToInferProportion(sP$kConfidence,
+                                  sP$maxProbPcodeHasMissingLinks, sP$kInterval)
+
+sP$pCodeIndices <- sample(length(data$pcodes), sP$missingLinksSampleSize)
+sP$pCodes <- data$pcodes[sP$pCodeIndices]
+
+print("Generating contributor_ids sample parameters...")
+# maxmum probability that a contributor will have 'misassigned records'
+# (i.e records for two different contributors are have the same contributor_id)
+sP$maxProbContributorHasMisassignedRecord <-
+                data$ContribIdsWithMultipleNamesCount() / data$maxContributorId
+
+# minimum number of contributor_ids to sample to be able to infer the
+# proportion that have 'misassigned records'
+sP$misassignedRecordsSampleSize <-
+  MinSampleSizeToInferProportion(sP$kConfidence,
+                      sP$maxProbContributorHasMisassignedRecord, sP$kInterval)
+
+sP$contributorIds <-
+  sample(data$maxContributorId, sP$misassignedRecordsSampleSize)
+
+print(GetoptLong::qq("Generating random sample of @{sP$missingLinksSampleSize} postal_codes"))
+pCodesSample <- filter(data$set, postal_code %in% sP$pCodes)
+pCodesSample <- select(pCodesSample,
+  postal_code, contributor_id, full_name, clean_first_last_name
+)
+pCodesSample <- pCodesSample[!duplicated(pCodesSample),]
+
+pCodesToReview <-
+  filter(tally(group_by(pCodesSample, postal_code)), n>1)$postal_code
+
+pCodesSample$review <- 0
+pCodesSample$review[pCodesSample$postal_code %in% pCodesToReview] <- 2
+pCodesSample <- arrange(pCodesSample, desc(review), postal_code)
+
+
+print(GetoptLong::qq("Generating random sample of @{sP$misassignedRecordsSampleSize} contibutor_ids"))
+contribIdsSample <- filter(data$set, contributor_id %in% sP$contributorIds)
+contribIdsSample <- select(contribIdsSample,
+  contributor_id, full_name, clean_first_last_name
+)
+contribIdsSample <- contribIdsSample[!duplicated(contribIdsSample),]
+
+cIdsToReview <-
+  filter(tally(group_by(contribIdsSample, contributor_id)), n>1)$contributor_id
+
+contribIdsSample$review <- 0
+contribIdsSample$review[contribIdsSample$contributor_id %in% cIdsToReview] <- 2
+contribIdsSample <- arrange(contribIdsSample, desc(review), contributor_id)
+
+print("Saving samples as CSV files...")
+write.csv(pCodesSample,
+  file=GetoptLong::qq("@{config$targetDir}/postal_code_sample.csv"), row.names=FALSE
+)
+write.csv(contribIdsSample,
+  file=GetoptLong::qq("@{config$targetDir}/contributor_id_sample.csv"), row.names=FALSE
+)
