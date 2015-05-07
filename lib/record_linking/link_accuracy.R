d29e21f19bb809277fea4a42d3e45248f0e555f9
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2015-05-07 13:08:31 -0400

    testing dataWrapper tallying functions

--- lib/record_linking/link_accuracy.R
+++ lib/record_linking/link_accuracy.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 ContributionsDataWrapper <- function(dataSet) {
   newObj <- within(list(), {
     set <- dataSet
--- lib/tests/test_link_accuracy.R
+++ lib/tests/test_link_accuracy.R
@@ -1,6 +1,25 @@
 source("../record_linking/link_accuracy.R")
 
-test_that("MinSampleSizeToInferProportion is implemented to return the correct value", {
+context("ContributionsDataWrapper")
+
+mockData <- data.frame(
+  postal_code=c("A0A0A1", "A0A0A2", "A0A0A2", "A0A0A3", "A0A0A3", "A0A0A3"),
+  contributor_id=c(1,12,123,13,13,13),
+  full_name=c("Michael Anthony", "David Lee Roth", "David Roth",
+                "Eddie Van Halen", "Eddie Halen", "Edith Valen")
+)
+
+test_that("TallyUniqueYPerX dependent methods are accurate", {
+  data <- ContributionsDataWrapper(mockData)
+
+  expect_equal(data$ContribIdsWithMultipleNamesCount(), 1)
+  expect_equal(data$PostalCodesWithMultipleContributorIdsCount(), 1)
+
+})
+
+context("Helpers")
+
+test_that("MinSampleSizeToInferProportion is accurate", {
 
   # In order to infer proportion to a 95% accurrate, 10 point interval, where
   # the probability of success for any trail is 80%, the inference must based on
